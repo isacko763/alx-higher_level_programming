f4499d8bc7a7b53808e70c5194b7e024bda61b6d
isacko763 <elemabarako6@gmail.com>
2023-09-11 10:04:32 -0700

    fvc7j

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-"""
-Contains the inherits_from function
-"""
+"""Module inherits_from
+Finds if the object is an instance of a class that inherited
+(directly/indirectly) from the specified class"""
 
 
 def inherits_from(obj, a_class):
-    """returns true if obj is a subclass of a_class, otherwise false"""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    """Determines if obj is an instance of a class that
+    inherited from a_class
+
+    Args:
+        - obj: object to look at
+        - a_class: class to evaluate
+
+    Returns: True or False
+    """
+
+    return isinstance(obj, a_class) and type(obj) != a_class
